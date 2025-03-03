7a5422c155cd5b5396907f81d24926ab29df46b6
SHASHWATTIWARI07 <shashwattiwari0709@gmail.com>
2025-03-03 12:26:34 +0530

    [Shashwat]Add : Check username is valid

--- user.py
+++ user.py
@@ -1,14 +1,19 @@
 import re
-def first_name(name):
+def compare_name(name):
     if re.match(r'^[A-Z][a-zA-Z]{2,}$', name):
         return True
     else:
         return False
 if __name__ == '__main__':
-    user_name = input("Enter your first name:")
-    check_name = first_name(user_name)
-    if check_name:
+    first_name = input("Enter your first name:")
+    last_name = input("Enter your last name:")
+    check_first_name = compare_name(first_name)
+    check_second_name = compare_name(last_name)
+    if check_first_name:
         print("first name is valid")
     else:
         print("first name is invalid")
-
+    if check_second_name:
+        print("last name is valid")
+    else:
+        print("last name is invalid")
